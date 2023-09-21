df9123a34015b06effff7f72d13129948959b1b5
n-devs <n.devs260340@gmail.com>
2023-09-22 03:26:41 +0700

    1.0.4

---
+++ template/example/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import {AppComponent} from 'test-lib';
+
+class App extends Component {
+  render() {
+    return <AppComponent text='Modern React component module' />;
+  }
+}
+
+export default App;
